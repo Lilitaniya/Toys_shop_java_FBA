494d72c7a3a11808d096764b17d5329df076897c
Bazhena <kagome-fortuna@ya.ru>
2024-02-24 00:50:19 +0300

    added toys_add

--- src/ToysShop.java
+++ src/ToysShop.java
@@ -11,3 +11,18 @@ public class ToysShop {
 
     // Генератор случайных чисел для выбора игрушек.
     private Random random;
+    public ToysShop(String[] toyData) {
+        // Инициализация очереди с приоритетом и генератора случайных чисел.
+        toys = new PriorityQueue<>();
+        random = new Random();
+
+        // Добавление игрушек в очередь с приоритетом.
+        for (String toy : toyData) {
+            String[] fields = toy.split(",");
+            int id = Integer.parseInt(fields[0]);
+            String name = fields[1];
+            int weight = Integer.parseInt(fields[2]);
+
+            toys.add(new Toy(id, name, weight));
+        }
+    }
