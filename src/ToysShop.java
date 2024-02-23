5bb256451945cc8cdb2025ae3ebb68c617a40f83
Bazhena <kagome-fortuna@ya.ru>
2024-02-24 00:48:57 +0300

    added random

--- src/ToysShop.java
+++ src/ToysShop.java
@@ -4,4 +4,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+public class ToysShop {
 
+    // Очередь с приоритетом для хранения игрушек. Игрушки упорядочены по весу, при этом игрушки с меньшим весом имеют более высокий приоритет.
+    private PriorityQueue<Toy> toys;
+
+    // Генератор случайных чисел для выбора игрушек.
+    private Random random;
